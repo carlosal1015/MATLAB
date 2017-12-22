7c541e1026ecb973111c4e9327bbf0c519a31827
Oromion <caznaranl@uni.pe>
2017-12-22 18:08:46 -0500

    Add files via upload

---
+++ Clase 5/clase5_10.m
@@ -0,0 +1,5 @@
+x=linspace(-pi, pi, 700);
+y=sin(x);
+%plot(x, y, 'r')
+plot(x, y, 'm*')
+grid
---
+++ Clase 5/clase5_9.m
@@ -0,0 +1,7 @@
+x=-10:10;
+%x es un vector, por lo tanto, cada elemento lo elevo al cuadrado.
+y=x.^3;
+plot(x, y)
+grid
+% Revisar el manual digital de colores.
+%https://www.mathworks.com/matlabcentral/answers/22261-how-to-insert-a-complex-root-in-a-text-using-fprintf
