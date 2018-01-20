c61875bcaf1875411c377bcf147b2074d603de1b
Oromion <caznaranl@uni.pe>
2018-01-20 11:31:24 -0500

    Initial commit

---
+++ MiríadaX/Módulo 1/ejercicio_1_5_1.m
@@ -0,0 +1,3 @@
+clear all, clc;
+r = input('Ingrese el radio del circulo (en metros): ');
+fprintf('El area de la region circular es %.3f metros cuadrados.\n', pi * r * r);
---
+++ MiríadaX/Módulo 1/ejercicio_1_5_3.m
@@ -0,0 +1,5 @@
+clear all, clc;
+edad = input('Por favor, ingrese su edad. (Por ejemplo: 15): ');
+fecha_act= input('Ingrese el año actual')
+nombre = 
+fprintf('.\n', 's');
